758ada28f2c7adcc1618be179e05ab1fffb97d82
storm37000 <darkrai37@hotmail.com>
2018-06-25 16:25:04 -0400

    initial commit

---
+++ EventHandler.cs
@@ -0,0 +1,24 @@
+﻿using Smod2;
+using Smod2.API;
+using Smod2.Events;
+using Smod2.EventHandlers;
+using System.Threading;
+
+namespace Smod.TestPlugin
+{
+    class EventHandler : IEventHandlerSpawn
+    {
+        private Plugin plugin;
+
+        public EventHandler(Plugin plugin)
+        {
+            this.plugin = plugin;
+        }
+
+        public void OnSpawn(PlayerSpawnEvent ev)
+        {
+            Thread plyjoindelaythread = new Thread(new ThreadStart(() => new playerjoindelaythread(this.plugin,ev)));
+            plyjoindelaythread.Start();
+        }
+    }
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# SCPSL_Smod_DefaultAmmo
